bb5b37f35275b7fe100571597d702d4331009aeb
Stephen Gregory <stejgregory@gmail.com>
2016-09-25 23:33:00 +0100

    We tried pairing through ScreenHero

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(number)
+  if (number % 3 == 0) && (number % 5 != 0)
+    "fizz"
+  elsif (number % 5 == 0) && (number % 3 != 0)
+    "buzz"
+  elsif (number % 3 == 0) && (number % 5 == 0)
+    "fizzbuzz"
+  else
+    number
+  end
+end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,32 @@
+require './lib/fizzbuzz'
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "buzz" when passed 25' do
+    expect(fizzbuzz(25)).to eq 'buzz'
+  end
+
+  it 'returns "buzz" when passed 27' do
+    expect(fizzbuzz(27)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "buzz" when passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
+
+
+end
